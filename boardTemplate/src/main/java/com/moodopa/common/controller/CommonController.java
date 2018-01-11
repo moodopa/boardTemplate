8235e942cac718af3f017dffca98a58651a3bf7e
moodopa <egglovery@gmail.com>
2018-01-11 11:50:42 +0900

    삭제
    삭제

--- boardTemplate/src/main/java/com/moodopa/common/controller/CommonController.java
+++
@@ -1,9 +0,0 @@
-package com.moodopa.common.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/main.do")
-public class CommonController {
-}
